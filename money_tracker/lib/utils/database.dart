6c6acc4f940372dda057054324f4daa2cf2ffe4c
iPott1337i <95004860+iPott1337i@users.noreply.github.com>
2021-11-26 16:56:10 -0600

    adding db and money model

---
+++ money_tracker/lib/utils/database.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBHelper {
+  DBHelper._privateConstructor();
+  static final DBHelper instance = DBHelper._privateConstructor();
+
+  static Database? _database;
+  Future<Database> get database async => _database ?? await _initDatabase();
+
+  Future<Database> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, 'experiments.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+      // onUpgrade: _onUpgrade,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE experiments(
+        id INTEGER PRIMARY KEY,
+        type INTEGER,
+        amount DOUBLE,
+        date DATE,
+        tag TEXT
+      );
+    ''');
+  }
+}
